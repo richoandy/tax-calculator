976e083063e5d05f981664a269b33d39d1e59d3e
richoandy <richoandy@gmail.com>
2019-01-10 20:44:52 +0700

    tidying up purchase test

--- tests/purchase.test.js
+++ tests/purchase.test.js
@@ -1,38 +1,38 @@
-const chai = require('chai')
-const chaiHttp = require('chai-http')
-const chaiSubset = require('chai-subset')
-const expect = chai.expect
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const chaiSubset = require('chai-subset');
+const expect = chai.expect;
 
-const app = require('../app')
-const Purchase = require('../models').Purchase
-const dummyData = require('./dummy.json')
+const app = require('../app');
+const Purchase = require('../models').Purchase;
+const dummyData = require('./dummy.json');
 
-chai.use(chaiHttp)
-chai.use(chaiSubset)
+chai.use(chaiHttp);
+chai.use(chaiSubset);
 
 //clean database
 before((done) => {
   Purchase.destroy({ where: {} })
     .then(function (response) {
-      done()
+      done();
     })
     .catch(function (err) {
-      console.log(err)
-      throw new Error('fail to clear table')
-      
-    })
-})
+      console.log(err);
+      throw new Error('fail to clear table');
+    });
+});
 
 //seed dummy data
 before((done) => {
   Purchase.bulkCreate(dummyData)
     .then(function (response) {
-      done()
+      done();
     })
     .catch(function (err) {
-      throw new Error('fail to seed dummy data')
-    })
-})
+      console.log(err);
+      throw new Error('fail to seed dummy data');
+    });
+});
 
 describe('-POST /purchases', function () {
   it ('should create new purchase succesfully', function (done) {
@@ -45,12 +45,12 @@ describe('-POST /purchases', function () {
         price: 10000
       })
       .end((err, res) => {
-        expect(res).to.have.status(201)
-        expect(res.body).to.ownProperty('name').equals('drink')
-        expect(res.body).to.ownProperty('tax_code').equals('1')
-        expect(res.body).to.ownProperty('price').equals(10000)
+        expect(res).to.have.status(201);
+        expect(res.body).to.ownProperty('name').equals('drink');
+        expect(res.body).to.ownProperty('tax_code').equals('1');
+        expect(res.body).to.ownProperty('price').equals(10000);
         done()
-      })
+      });
   })
   it ('should fail to create new purchase with blank/null name field, tax code > 3 or tax code < 1, price equals 0', function (done) {
     chai
@@ -61,15 +61,15 @@ describe('-POST /purchases', function () {
         price: 0
       })
       .end((err, res) => {
-        expect(res).to.have.status(400)
-        expect(res.body).to.ownProperty('name').equals('SequelizeValidationError')
-        expect(res.body.errors[0].message).equals('Purchase.name cannot be null')
-        expect(res.body.errors[1].message).equals('tax code 1, 2, 3 only allowed')
-        expect(res.body.errors[2].message).equals('minimum price is 1')
-        done()
+        expect(res).to.have.status(400);
+        expect(res.body).to.ownProperty('name').equals('SequelizeValidationError');
+        expect(res.body.errors[0].message).equals('Purchase.name cannot be null');
+        expect(res.body.errors[1].message).equals('tax code 1, 2, 3 only allowed');
+        expect(res.body.errors[2].message).equals('minimum price is 1');
+        done();
       })
-  })
-})
+  });
+});
 
 describe('-GET /purchases', function () {
   it ('should get all purchases succesfully', function (done) {
@@ -77,37 +77,36 @@ describe('-GET /purchases', function () {
     .request(app)
     .get('/purchases')
     .end((err, res) => {
-      expect(res).to.have.status(200)
-      expect(res.body).to.ownProperty('totals').to.be.an('object')
-      expect(res.body).to.ownProperty('data').to.be.an('array')
-      expect(res.body.data).to.have.length(5)
-      expect(res.body.data[0]).to.have.keys('id', 'name', 'tax_code', 'price', 'createdAt', 'updatedAt', 'tax', 'amount', 'type', 'refundable')
+      expect(res).to.have.status(200);
+      expect(res.body).to.ownProperty('totals').to.be.an('object');
+      expect(res.body).to.ownProperty('data').to.be.an('array');
+      expect(res.body.data).to.have.length(5);
       //totals
-      expect(res.body.totals.grandTotal).equals(13159)
-      expect(res.body.totals.totalTax).equals(1049)
-      expect(res.body.totals.totalPrice).equals(12110)
+      expect(res.body.totals.grandTotal).equals(13159);
+      expect(res.body.totals.totalTax).equals(1049);
+      expect(res.body.totals.totalPrice).equals(12110);
       // Food & Beverages Type
-      expect(res.body.data[0].tax).equals(10)
-      expect(res.body.data[0].amount).equals(110)
-      expect(res.body.data[0].refundable).to.be.true
-      expect(res.body.data[0].type).equals('Food & Beverages')
+      expect(res.body.data[0].tax).equals(10);
+      expect(res.body.data[0].amount).equals(110);
+      expect(res.body.data[0].refundable).to.be.true;
+      expect(res.body.data[0].type).equals('Food & Beverages');
       // Tobacco Type
-      expect(res.body.data[1].tax).equals(30)
-      expect(res.body.data[1].amount).equals(1030)
-      expect(res.body.data[1].refundable).to.be.false
-      expect(res.body.data[1].type).equals('Tobacco')
+      expect(res.body.data[1].tax).equals(30);
+      expect(res.body.data[1].amount).equals(1030);
+      expect(res.body.data[1].refundable).to.be.false;
+      expect(res.body.data[1].type).equals('Tobacco');
       // Entertainment Type < 100
-      expect(res.body.data[2].tax).equals(0)
-      expect(res.body.data[2].amount).equals(10)
-      expect(res.body.data[2].refundable).to.be.false
-      expect(res.body.data[2].type).equals('Entertainment')
+      expect(res.body.data[2].tax).equals(0);
+      expect(res.body.data[2].amount).equals(10);
+      expect(res.body.data[2].refundable).to.be.false;
+      expect(res.body.data[2].type).equals('Entertainment');
       // Entertainment Type > 100
-      expect(res.body.data[3].tax).equals(9)
-      expect(res.body.data[3].amount).equals(1009)
-      expect(res.body.data[3].refundable).to.be.false
-      expect(res.body.data[3].type).equals('Entertainment')
-      done()
-    })
-  })
-})
+      expect(res.body.data[3].tax).equals(9);
+      expect(res.body.data[3].amount).equals(1009);
+      expect(res.body.data[3].refundable).to.be.false;
+      expect(res.body.data[3].type).equals('Entertainment');
+      done();
+    });
+  });
+});
 
